b9c39f2a72aadfc85dc4840a041cb8d36540e746
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-12-26 22:44:11 +0200

    add text

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View ,TextInput } from 'react-native';
 export default function App() {
+  const {text, onChangetext} = useState('useless text')
   console.log("hi i begin")
   return (
     <View style={styles.container}>
       <Text>ali Tamrawe is here now </Text>
       <Text>next text </Text>
-
+      <TextInput 
+       style={styles.input}
+       onChangeText={onChangetext}
+       value={text}
+       /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -14,10 +20,18 @@ export default function App() {
 
 
 const styles = StyleSheet.create({
+  input: {
+    width:200,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#afe',
+    flex: 2,
+    backgroundColor: '#088',
     alignItems: 'center',
+    left:2,
     justifyContent: 'center',
   },
 });
